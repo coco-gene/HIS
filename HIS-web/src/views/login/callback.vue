32b8254b5219598eafe3f724b0de446816adf9da
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-04-29 16:56:10 +0800

    update

--- HIS-web/src/views/login/callback.vue
+++ HIS-web/src/views/login/callback.vue
@@ -1,12 +1,20 @@
 <script>
 export default {
-  name: 'AuthRedirect',
+  name: 'Callback',
   created() {
-    const hash = window.location.search.slice(1)
-    console.log(hash)
-    console.log(window.location.search)
+    const href = window.location.href // 获取当前url
+    // const path = href.split('#')[0] // 获取#/之前的字符串
+    const argumentStr = href.split('?')[1] // 获取?之后的参数字符串
+    const argumentArr = argumentStr.split('&') // 参数字符串分割为数组
+    let argument
+    const data = {}
+    for (var i = 0; i < argumentArr.length; i++) { // 遍历数组，拿到json对象
+      argument = argumentArr[i].split('=')
+      data[argument[0]] = argument[1]
+    }
+    console.log(data)
     if (window.localStorage) {
-      window.localStorage.setItem('x-admin-oauth-code', hash)
+      window.localStorage.setItem('x-admin-oauth-code', data)
       // window.close()
     }
   },
